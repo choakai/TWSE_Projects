22648deb0fc5cb95ae04b8c062a3537fc9485252
kk <kk@1332.tw>
2017-04-12 23:18:12 +0800

    20170412

--- Client_Socket/Form1.cs
+++ Client_Socket/Form1.cs
@@ -8,69 +8,98 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net.Sockets;
+using System.Diagnostics;// 匯入System.Diagnostics 命名空間
 
 namespace Client_Socket
-
 {
-
     public partial class Form1 : Form
-
     {
-
-        System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient();
-
-
+        private PerformanceCounter ProcessorUsage = new PerformanceCounter("Processor", "% Processor Time", "_Total");//定義CPU效能計數器
+        //System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient();
 
         public Form1()
-
         {
-
             InitializeComponent();
-
         }
-
-
 
         private void Form1_Load(object sender, EventArgs e)
-
         {
-
-            msg("Client Started");
-
-            clientSocket.Connect("127.0.0.1", 8888);
-
             //label1.Text = "Client Socket Program - Server Connected ...";
-
         }
-
-
 
         private void button1_Click_1(object sender, EventArgs e)
 
         {
+            while (true)
+            {
+                try
+                {
 
-            NetworkStream serverStream = clientSocket.GetStream();
+                    msg("Client Started");
+                    System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient();
+                    clientSocket.NoDelay = true;
+                    int intBreakFlag = 0;
+                    for (int i=0;i<3;i++)
+                    {
+                        try
+                        {
+                            clientSocket.Connect("127.0.0.1", 8888);
+                            clientSocket.ReceiveTimeout = 1000;
+                            break;
+                        }
+                        catch(Exception ex)
+                        {
+                            //Console.WriteLine("ReTry Connection..."+ex.Message);
 
-            byte[] outStream = System.Text.Encoding.ASCII.GetBytes(textBox2.Text + "$");
+                            if (i<3)
+                            {
+                                intBreakFlag = 1;
+                            }
+                        }
+                    }
+                    if (intBreakFlag == 1)
+                    {
+                        Console.WriteLine("ReTry 3 times , exit.");
+                        break;
+                    }
 
-            serverStream.Write(outStream, 0, outStream.Length);
+                    NetworkStream serverStream = clientSocket.GetStream();
 
-            serverStream.Flush();
+                    //string szCPU = decimal.Round(decimal.Parse(ProcessorUsage.NextValue().ToString()), 0, MidpointRounding.ToEven).ToString();
 
+                    Console.WriteLine(ProcessorUsage.NextValue());
+                    System.Threading.Thread.Sleep(333);
+                    string szCPU = string.Format("{0:n1}", ProcessorUsage.NextValue());
+                    Console.WriteLine(szCPU);
+                    //byte[] outStream = System.Text.Encoding.ASCII.GetBytes(textBox2.Text + "$");
+                    byte[] outStream = System.Text.Encoding.ASCII.GetBytes(szCPU + "$");
 
+                    serverStream.Write(outStream, 0, outStream.Length);
 
-            byte[] inStream = new byte[10025];
+                    serverStream.Flush();
 
-            serverStream.Read(inStream, 0, (int)clientSocket.ReceiveBufferSize);
+                    byte[] inStream = new byte[10025];
 
-            string returndata = System.Text.Encoding.ASCII.GetString(inStream);
+                    serverStream.Read(inStream, 0, (int)clientSocket.ReceiveBufferSize);
 
-            msg(returndata);
+                    string returndata = System.Text.Encoding.ASCII.GetString(inStream);
 
-            textBox2.Text = "";
+                    msg(returndata);
 
-            textBox2.Focus();
+                    textBox1.Text = "";
 
+                    textBox1.Focus();
+                    
+                    clientSocket.Close();
+                    clientSocket = null;
+                    System.Threading.Thread.Sleep(333);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+
+                }
+            }
         }
 
 
@@ -79,7 +108,7 @@ namespace Client_Socket
 
         {
 
-            textBox1.Text = textBox1.Text + Environment.NewLine + " >> " + mesg;
+            textBox2.Text = textBox1.Text + Environment.NewLine + " >> " + mesg;
 
         }
 
--- Server_Socket/Program.cs
+++ Server_Socket/Program.cs
@@ -4,14 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Net.Sockets;
 using System.Threading;
+using Server_Socket.Utilities;
+using System.Data.SQLite;
 namespace Server_Socket
 {
     class Program
     {
         static void Main(string[] args)
         {
+            while ((true))
+            {
+                try
+                {
+                    
+                    System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();//引用stopwatch物件
+                    sw.Reset();//碼表歸零
+                    sw.Start();//碼表開始計時
+                    openTcpListener();
+                    sw.Stop();//碼錶停止
+                              //印出所花費的總豪秒數
+                    string result1 = (sw.Elapsed.TotalMilliseconds/1000).ToString();
+                    Console.WriteLine(result1);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            
+        }
 
-            TcpListener serverSocket = new TcpListener(System.Net.IPAddress.Parse("127.0.0.1"),8888);
+        public static void openTcpListener()
+        {
+            Int32 intPort = 0;
+            Int32.TryParse(Setting.SERVER_PORT.ToString(), out intPort);
+
+            Utilities.Schedule Schedule = new Utilities.Schedule();
+            bool bolSch = Schedule.Schedule_Start();
+
+            TcpListener serverSocket = new TcpListener(System.Net.IPAddress.Parse(Setting.SERVER_IP.ToString()),intPort);
             int requestCount = 0;
             TcpClient clientSocket = default(TcpClient);
             serverSocket.Start();
@@ -35,22 +66,25 @@ namespace Server_Socket
                     Byte[] sendBytes = Encoding.ASCII.GetBytes(serverResponse);
                     networkStream.Write(sendBytes, 0, sendBytes.Length);
                     networkStream.Flush();
+                    Console.WriteLine("============================================================");
                     Console.WriteLine(" >> " + serverResponse);
+                    Console.WriteLine("============================================================");
                     //Thread.Sleep(100);
                 }
                 catch (Exception ex)
                 {
                     Console.WriteLine(ex.ToString());
+                    break;
                 }
+                break;
             }
 
             //clientSocket.Close();
             clientSocket = null;
             serverSocket.Stop();
             Console.WriteLine(" >> exit");
-            Console.ReadLine();
+            //Console.ReadLine();
         }
-
     }
 }
 
